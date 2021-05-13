eb7406a44afa7cd14a1e6d89726340944d5ea584
robledojuanse <42225602+robledojuanse@users.noreply.github.com>
2021-05-12 21:12:42 -0400

    Se sube proyecto

---
+++ dotnetcore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace dotnetcore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string testData(string str)
+        {
+
+            return str + "test";
+        }
+    }
+}
